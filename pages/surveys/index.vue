88c6ce32ec7ff47aa1919a3a31e7b6ff711feba5
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-10 03:24:18 +0900

    Styling surveys

--- pages/surveys/index.vue
+++ pages/surveys/index.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="container mx-auto">
-    <div>
-      <ul>
+  <div class="container mx-auto mt-4 px-4">
+    <div class="mb-8">
+      <h1 class="text-4xl text-bold border-b border-black mb-4">공개설문</h1>
+      <ul class="flex flex-wrap">
         <nuxt-link
           v-for="survey in publicSurveys"
           :key="survey._id"
           tag="li"
           :to="{ name: 'surveys-id', params: { id: survey._id } }"
+          class="border border-gray-700 rounded cursor-pointer hover:bg-black hover:text-white"
         >
-          {{ survey._id }} - {{ survey.title }} <button @click="remove(survey._id)">X</button>
+          <div class="px-4 py-2">
+            <div class="text-xl">{{ survey.title }}</div>
+            <div class="">{{ survey.description }}</div>
+          </div>
+          <div class="py-2 px-4">
+            <span class="text-sm">{{ survey.questions.length }}개 질문</span>&nbsp;/&nbsp;
+            <span class="text-sm">{{ survey.participantsCount }}회 참여</span>
+          </div>
         </nuxt-link>
       </ul>
     </div>
-    <hr>
     <div>
-      <ul>
+      <h1 class="text-4xl text-bold border-b border-black mb-2">비공개설문</h1>
+      <ul class="flex flex-wrap">
         <nuxt-link
           v-for="survey in privateSurveys"
           :key="survey._id"
           tag="li"
           :to="{ name: 'surveys-id', params: { id: survey._id } }"
+          class="border border-gray-700 rounded cursor-pointer hover:bg-black hover:text-white"
         >
-          {{ survey._id }} - {{ survey.title }}  :: {{ survey.author }} <button @click="remove(survey._id)">X</button>
+          <div class="px-4 py-2">
+            <div class="text-xl">{{ survey.title }}</div>
+            <div class="">{{ survey.description }}</div>
+          </div>
+          <div class="py-2 px-4">
+            <span class="text-sm">{{ survey.questions.length }}개 질문</span>&nbsp;/&nbsp;
+            <span class="text-sm">{{ survey.participantsCount }}회 참여</span>
+          </div>
         </nuxt-link>
       </ul>
     </div>
