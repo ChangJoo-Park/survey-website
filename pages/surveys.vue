d7994cdf9a197cc28a55eb8a3ab7df3e9809c353
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-10 03:05:36 +0900

    Update app navbar

--- pages/surveys.vue
+++ pages/surveys.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="">
-    <div class="flex items-center px-4" style="height: 3rem;">
-      <nuxt-link :to="{ name: 'surveys' }">HOME</nuxt-link>&nbsp;
-      <nuxt-link :to="{ name: 'surveys-create' }">NEW SURVEY</nuxt-link>&nbsp;
-      <button @click="signout">SignOut</button>
+    <div class="flex items-center px-4 bg-black" style="height: 3rem;">
+      <div class="flex-1">
+        <nuxt-link :to="{ name: 'index' }" class="text-white font-mono mr-8">MoleculeSurvey</nuxt-link>&nbsp;
+        <nuxt-link :to="{ name: 'surveys' }" class="text-white px-2 py-2">대시보드</nuxt-link>&nbsp;
+        <nuxt-link :to="{ name: 'surveys-create' }" class="text-white px-2 py-2">새 설문 만들기</nuxt-link>&nbsp;
+      </div>
+      <div>
+        <nuxt-link :to="{ name: 'surveys-me' }" class="text-white px-2 py-2">{{ loggedInUser.username }}</nuxt-link>&nbsp;
+        <button @click="signout" class="text-white px-2 py-2">로그아웃</button>
+      </div>
     </div>
     <nuxt-child />
   </div>
 </template>
 
 <script>
-import { mapActions } from 'Vuex';
+import { mapActions, mapGetters } from 'Vuex';
 
 export default {
+  computed: {
+    ...mapGetters(['loggedInUser'])
+  },
   methods: {
     ...mapActions(['setUser', 'setAccessToken']),
     signout () {
