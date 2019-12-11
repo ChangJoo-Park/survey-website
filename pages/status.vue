d7a7a2588a2396fc1064f5260c1d8e8c574c8efa
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-11 21:42:20 +0900

    Add status page

--- pages/status.vue
+++ pages/status.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-
+    <div>
+      CPU : {{ cpu }}
+    </div>
+    <div>
+      MEMORY : {{ mem }}
+    </div>
+    <div>
+      OS : {{ os }}
+    </div>
+    <div>
+      PROCESS : {{ process }}
+    </div>
+    <div>
+      TIME : {{ time }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  async asyncData ({ app, query, req, params }) {
+    const { data } = await app.$axios({
+      url: `http://localhost:3000/health`,
+    })
+    return {
+      ...data
+    }
+  }
 }
 </script>
 
