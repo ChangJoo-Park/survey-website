3d7bbcfb21078422d60ec3d8debfffae229bfafb
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-12 10:27:55 +0900

    Update me page

--- pages/surveys/me.vue
+++ pages/surveys/me.vue
@@ -1,10 +1,15 @@
 <template>
-  <div></div>
+  <div class="container mx-auto">
+    {{ loggedInUser }}
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
-
+  computed: {
+    ...mapGetters(['loggedInUser'])
+  }
 }
 </script>
 
