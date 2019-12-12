90cefeca397e356554671d1de8e57587c66f03ef
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-12 22:37:52 +0900

    Add notifications

--- pages/surveys/_id.vue
+++ pages/surveys/_id.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="container mx-auto mt-8 flex">
-    <aside class="p-4 mr-4" style="min-width: 200px;">
+    <aside class="px-4 pb-4 mr-4" style="min-width: 200px;">
       <nav class="">
         <ul>
           <nuxt-link
-            tag="li"
             :to="{ name: 'surveys-id', params: { id: $route.params.id }}"
-            class="subnav--link text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
+            class="subnav--link block text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
           >통계</nuxt-link>
           <nuxt-link
-            tag="li"
+            :to="{ name: 'surveys-id-edit', params: { id: $route.params.id }}"
+            class="subnav--link block text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
+          >수정하기</nuxt-link>
+          <nuxt-link
             :to="{ name: 'surveys-id-participations', params: { id: $route.params.id }}"
-            class="subnav--link text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
+            class="subnav--link block text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
           >참여목록</nuxt-link>
           <nuxt-link
-            tag="li"
             :to="{ name: 'surveys-id-share', params: { id: $route.params.id }}"
-            class="subnav--link text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
+            class="subnav--link block text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
           >공유하기</nuxt-link>
           <nuxt-link
-            tag="li"
-            :to="{ name: 'surveys-id-edit', params: { id: $route.params.id }}"
-            class="subnav--link text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
-          >수정하기</nuxt-link>
+            :to="{ name: 'surveys-id-notifications', params: { id: $route.params.id }}"
+            class="subnav--link block text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
+          >알림설정</nuxt-link>
+          <li class="border-b w-full mb-2"></li>
+          <nuxt-link
+            :to="{ name: 'take-id', params: { id: $route.params.id }}"
+            target="_blank"
+            class="subnav--link block text-gray-700 cursor-pointer px-2 hover:underline text-lg mb-2"
+          >미리보기</nuxt-link>
         </ul>
       </nav>
     </aside>
