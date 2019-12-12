e4d68ad2b8bd4e112f403693c8940b73d46f5738
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-12 13:04:12 +0900

    Update surveys nav

--- pages/surveys.vue
+++ pages/surveys.vue
@@ -2,18 +2,25 @@
   <div>
     <div class="bg-black sticky">
       <div class="flex items-center px-4 container mx-auto" style="height: 3rem;">
-        <div class="flex-1">
+        <div class="flex flex-row items-center flex-1">
           <nuxt-link :to="{ name: 'index' }" class="text-white font-mono mr-8">Survey</nuxt-link>&nbsp;
           <nuxt-link :to="{ name: 'surveys' }" class="nav--link">대시보드</nuxt-link>&nbsp;
           <nuxt-link :to="{ name: 'surveys-list' }" class="nav--link">설문현황</nuxt-link>&nbsp;
-          <nuxt-link :to="{ name: 'surveys-create' }" class="nav--link">설문만들기</nuxt-link>&nbsp;
+          <nuxt-link :to="{ name: 'surveys-create' }" class="nav--link mr-8">설문만들기</nuxt-link>&nbsp;
+          <form class="" @submit.prevent="search">
+            <input
+              type="search"
+              class="search--input"
+              style="min-width: 300px;"
+              placeholder="설문을 찾아보세요"
+              v-model.trim="searchTerm"
+            >
+          </form>
         </div>
         <div class="flex items-center">
-          <form class="" @submit.prevent="search">
-            <input type="search" class="search--input" placeholder="설문을 찾아보세요" v-model="searchTerm">
-          </form>
-          <nuxt-link :to="{ name: 'surveys-me' }" class="nav--link">{{ loggedInUser.username }}</nuxt-link>&nbsp;
-          <button @click="signout" class="nav--link">로그아웃</button>
+          <!-- <nuxt-link :to="{ name: 'surveys-me' }" class="nav--link">{{ loggedInUser.username }}</nuxt-link>&nbsp; -->
+          <account-dropdown />
+          <!-- <button @click="signout" class="nav--link">로그아웃</button> -->
         </div>
       </div>
     </div>
@@ -24,8 +31,12 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import AccountDropdown from '~/components/account-dropdown.vue'
 
 export default {
+  components: {
+    AccountDropdown
+  },
   async asyncData ({ app, query, req, params, route }) {
     if (route.name === 'surveys-search' && query.term) {
       return { searchTerm: query.term }
