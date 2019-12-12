7b33cba571dc1faca94cd5c1d27859c16d0fabfc
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-12 10:24:03 +0900

    Add search page

--- pages/surveys/search.vue
+++ pages/surveys/search.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    {{ surveys }}
+  <div class="container mx-auto mt-4">
+    <h1 class="text-xl mb-2">총 {{ surveys.length }}개의 설문</h1>
+    <ul>
+      <li v-for="item in surveys" :key="item._id" class="border-b mb-4">
+        <div>
+          {{ item.title }}
+        </div>
+        <div>
+          {{ item.description }}
+        </div>
+        <div>
+          {{ item.questions }}
+        </div>
+        <div>
+          {{ item.createdAt }}
+        </div>
+        <div>
+          {{ item.updatedAt }}
+        </div>
+        <div>
+          {{ item.participantsCount }}
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,9 +30,15 @@
 export default {
   async asyncData ({ app, query, req, params }) {
     const { term = '' }  = query
+    if (term === '') {
+      return {
+        surveys: [], term
+      }
+    }
+
     const { data: surveys } = await app.$axios.get('/me/surveys/search', {params: { term }})
     return {
-      surveys
+      surveys, term
     }
   },
   watch: {
