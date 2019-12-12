4d405b23cda6928bf24ba1fe902450cab1bce2e3
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-12-12 10:37:18 +0900

    Add link for share

--- pages/surveys/_id.vue
+++ pages/surveys/_id.vue
@@ -10,6 +10,10 @@
         class="py-1 px-2 underline"
       >참여목록</nuxt-link>
       <nuxt-link
+        :to="{ name: 'surveys-id-share', params: { id: $route.params.id }}"
+        class="py-1 px-2 underline"
+      >공유하기</nuxt-link>
+      <nuxt-link
         :to="{ name: 'surveys-id-edit', params: { id: $route.params.id }}"
         class="py-1 px-2 underline"
       >수정하기</nuxt-link>
